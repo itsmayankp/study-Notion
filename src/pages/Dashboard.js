8be10c932fe633531345cf41fabdb7de521ae6fc
Mayank Pandey <immayank7781@gmail.com>
2023-07-30 23:48:57 +0530

    updated code

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard(){
+
+    return (
+        <div className="w-screen h-screen text-white text-3xl flex justify-center items-center">Welcome to Study Notion!</div>
+    );
+
+}
+
+export default Dashboard;
