fd9d5b1ea117f3837c66e37d4bb94f5b4e93779c
Mayank Pandey <immayank7781@gmail.com>
2023-07-30 23:50:46 +0530

    Login.js

---
+++ src/pages/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Template from "../components/Template";
+import loginImg from "../assets/login.png"
+
+function Login({setLoggedIn}){
+
+    return(
+        <Template
+            title= "Welcome Back"
+            description = "Build skills for today, tomorrow, and beyond."
+            desc1="Build skills for today, tomorrow, and beyond."
+            desc2="Education to future-proof your career."
+            image={loginImg}
+            formtype="login"
+            setLoggedIn={setLoggedIn}
+        />
+    );
+
+
+
+}
+
+export default Login;
