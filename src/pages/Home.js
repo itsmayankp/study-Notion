3fcd787232ff59551c70200b870b83eb17c1175d
Mayank Pandey <immayank7781@gmail.com>
2023-07-30 23:50:09 +0530

    Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+
+    return (
+        <div className="w-screen h-screen text-white text-3xl flex justify-center items-center">Home</div>
+    );
+
+
+
+}
+
+export default Home;
